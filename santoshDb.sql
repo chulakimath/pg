cff3b08b530efaf051a4f44ea25bacafaa318be3
chulakimath <santoshchulakimath222@gmail.com>
2026-03-08 17:08:38 +0530

    22

--- santoshDb.sql
+++ santoshDb.sql
@@ -8,24 +8,24 @@ SET search_path TO students;
 -- set name='santosh3',age=25 WHERE id = 6
 -- RETURNING *;
 
-SELECT * FROM students;
+-- SELECT * FROM students;
 
 
 -- CREATE SCHEMA flipkart_db;
 
-CREATE TABLE flipkart_db.products(
-    product_id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    sku_code CHAR(8) UNIQUE NOT NULL CHECK(CHAR_LENGTH(sku_code)=8),
-    price NUMERIC(10,2) NOT NULL CHECK(price>=0),
-    stock_quantity INT NOT NULL DEFAULT 0 CHECK (stock_quantity>=0),
-    is_available BOOLEAN NOT NULL DEFAULT TRUE,
-    category TEXT NOT NULL,
-    added_on DATE NOT NULL DEFAULT CURRENT_DATE,
-    last_updated TIMESTAMP DEFAULT now()
-)
+-- CREATE TABLE flipkart_db.products(
+--     product_id SERIAL PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL,
+--     sku_code CHAR(8) UNIQUE NOT NULL CHECK(CHAR_LENGTH(sku_code)=8),
+--     price NUMERIC(10,2) NOT NULL CHECK(price>=0),
+--     stock_quantity INT NOT NULL DEFAULT 0 CHECK (stock_quantity>=0),
+--     is_available BOOLEAN NOT NULL DEFAULT TRUE,
+--     category TEXT NOT NULL,
+--     added_on DATE NOT NULL DEFAULT CURRENT_DATE,
+--     last_updated TIMESTAMP DEFAULT now()
+-- )
 
-SET search_path TO flipkart_db
+-- SET search_path TO flipkart_db
 
 -- INSERT INTO
 --     flipkart_db.products (name,sku_code,price,stock_quantity,category)
@@ -55,8 +55,8 @@ SET search_path TO flipkart_db
 
 
 
-SELECT * 
-    FROM flipkart_db.products;
+-- SELECT * 
+--     FROM flipkart_db.products;
 
 -- SELECT last_value FROM flipkart_db.products_product_id_seq;
 -- SELECT setval('products_product_id_seq',15)
@@ -65,34 +65,34 @@ SELECT *
 
 -- DROP TABLE flipkart_db.products
 
-SELECT *
-    FROM 
-        flipkart_db.products
-    WHERE 
-        products.stock_quantity <> (
-            SELECT DISTINCT products.stock_quantity 
-                FROM 
-                    flipkart_db.products
-                ORDER BY products.stock_quantity DESC
-                LIMIT 1
-                OFFSET 1 
+-- SELECT *
+--     FROM 
+--         flipkart_db.products
+--     WHERE 
+--         products.stock_quantity <> (
+--             SELECT DISTINCT products.stock_quantity 
+--                 FROM 
+--                     flipkart_db.products
+--                 ORDER BY products.stock_quantity DESC
+--                 LIMIT 1
+--                 OFFSET 1 
 
-        )
-    ORDER BY sku_code DESC 
+--         )
+--     ORDER BY sku_code DESC 
 
 
 
-SELECt * FROM
-(SELECT *,dense_rank() OVER (ORDER BY products.stock_quantity DESC) AS qty_rank FROM flipkart_db.products) as tab
-WHERE tab.qty_rank=2
+-- SELECt * FROM
+-- (SELECT *,dense_rank() OVER (ORDER BY products.stock_quantity DESC) AS qty_rank FROM flipkart_db.products) as tab
+-- WHERE tab.qty_rank=2
 
-SELECT * FROM flipkart_db.products WHERE LOWER(flipkart_db.products.name) like '%st%'
-SELECT * FROM flipkart_db.products WHERE flipkart_db.products.name Ilike '%st%'
+-- SELECT * FROM flipkart_db.products WHERE LOWER(flipkart_db.products.name) like '%st%'
+-- SELECT * FROM flipkart_db.products WHERE flipkart_db.products.name Ilike '%st%'
 
-SELECT category,price
-    FROM 
-    (SELECT *, dense_rank() OVER (PARTITION BY category ORDER BY price DESC) AS rank FROM flipkart_db.products) AS TTABLE
-    WHERE rank <=2
+-- SELECT category,price
+--     FROM 
+--     (SELECT *, dense_rank() OVER (PARTITION BY category ORDER BY price DESC) AS rank FROM flipkart_db.products) AS TTABLE
+--     WHERE rank <=2
 
 
 --  SELECT  name,price
@@ -141,7 +141,87 @@ SELECT category,price
 
 
 -- SELECT 
---     DISTINCT UPPER(products.category) 
---     FROM 
---         flipkart_db.products
+--     UPPER(products.category) AS upp,
+--     LOWER(products.category) AS low,
+--     LENGTH(products.category)
+-- FROM 
+--     flipkart_db.products
+-- GROUP BY products.category
     
+
+-- SELECT 
+--     products.category
+-- FROM 
+--     flipkart_db.products
+
+-- Use EXPLAIN ANALYZE to diagnose query performance
+-- EXPLAIN ANALYZE
+-- SELECT 
+--     Upper(products.category)
+-- FROM 
+--     flipkart_db.products;
+
+
+
+
+
+
+-- EXPLAIN (ANALYZE,BUFFERS, FORMAT JSON)
+-- SELECT 
+--     flipkart_db.products.*
+-- FROM 
+--     flipkart_db.products
+
+
+-- EXPLAIN (ANALYZE,BUFFERS, FORMAT JSON)
+-- SELECT 
+--     CAST(cast_len AS INT)
+--     FROM (
+
+--         SELECT 
+--             length(len) AS cast_len
+--         FROM (
+--             SELECT 
+--                 CAST((LENGTH(flipkart_db.products.name))AS VARCHAR) AS len,
+--                 flipkart_db.products.name
+--             FROM 
+--                 flipkart_db.products
+--             )
+--     )
+
+
+-- SELECT 
+--     SUBSTR(products.sku_code,LENGTH(sku_code)-3,LENGTH(sku_code))
+-- FROM
+--     flipkart_db.products
+
+
+-- SELECT
+--     LEFT(products.sku_code,4) as LEFT,
+--     SUBSTR(products.sku_code,LENGTH(sku_code)-3,LENGTH(sku_code))
+-- FROM
+--     flipkart_db.products
+
+-- SELECT 
+--     CONCAT(products.sku_code,' - ',products.category,' - ',products.price) as sku_cat,
+--     CONCAT_WS(' - ',products.sku_code,products.category,products.price) as comcatWITHSEPERATOR
+-- FROM 
+--     flipkart_db.products
+
+
+
+
+
+1234
+
+
+
+
+
+
+
+SELECT 
+    TRIM('            Santosh  '),
+    ('            Santosh  ')
+
+
